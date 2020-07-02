a4e86120af6ae3e9b01d6ef67d44725bdeacf6e0
Nick Schwaderer <nschwaderer@chef.io>
2020-07-02 15:14:34 +0100

    temporary cop disable

--- lib/rubocop/cop/inspecstyle/deprecated_attributes.rb
+++ lib/rubocop/cop/inspecstyle/deprecated_attributes.rb
@@ -1,52 +1,52 @@
-# frozen_string_literal: true
-
-module RuboCop
-  module Cop
-    module InSpecStyle
-      # Checks if deprecated method attribute is used.
-      #
-      # @example EnforcedStyle: InSpecStyle (default)
-      #   # Attributes have been deprecated for inputs
-      #   # https://github.com/inspec/inspec/issues/3802
-      #
-      #   # bad
-      #   attribute('my_element', value: 10)
-      #
-      #   # good
-      #   input('my_element', value: 10)
-      #
-      class DeprecatedAttributes < Cop
-        include RangeHelp
-
-        MSG = 'Use `#input` instead of `#attribute`. This will be removed in '\
-              'InSpec 5'
-
-        def_node_matcher :attribute?, <<~PATTERN
-          (send nil? :attribute ...)
-        PATTERN
-
-        def on_send(node)
-          return unless attribute?(node)
-
-          add_offense(node, location: node.loc.selector)
-        end
-
-        def autocorrect(node)
-          lambda do |corrector|
-            corrector.replace(offense_range(node), preferred_replacement)
-          end
-        end
-
-        private
-
-        def offense_range(node)
-          node.loc.selector
-        end
-
-        def preferred_replacement
-          cop_config.fetch('PreferredReplacement')
-        end
-      end
-    end
-  end
-end
+# # frozen_string_literal: true
+#
+# module RuboCop
+#   module Cop
+#     module InSpecStyle
+#       # Checks if deprecated method attribute is used.
+#       #
+#       # @example EnforcedStyle: InSpecStyle (default)
+#       #   # Attributes have been deprecated for inputs
+#       #   # https://github.com/inspec/inspec/issues/3802
+#       #
+#       #   # bad
+#       #   attribute('my_element', value: 10)
+#       #
+#       #   # good
+#       #   input('my_element', value: 10)
+#       #
+#       class DeprecatedAttributes < Cop
+#         include RangeHelp
+#
+#         MSG = 'Use `#input` instead of `#attribute`. This will be removed in '\
+#               'InSpec 5'
+#
+#         def_node_matcher :attribute?, <<~PATTERN
+#           (send nil? :attribute ...)
+#         PATTERN
+#
+#         def on_send(node)
+#           return unless attribute?(node)
+#
+#           add_offense(node, location: node.loc.selector)
+#         end
+#
+#         def autocorrect(node)
+#           lambda do |corrector|
+#             corrector.replace(offense_range(node), preferred_replacement)
+#           end
+#         end
+#
+#         private
+#
+#         def offense_range(node)
+#           node.loc.selector
+#         end
+#
+#         def preferred_replacement
+#           cop_config.fetch('PreferredReplacement')
+#         end
+#       end
+#     end
+#   end
+# end
